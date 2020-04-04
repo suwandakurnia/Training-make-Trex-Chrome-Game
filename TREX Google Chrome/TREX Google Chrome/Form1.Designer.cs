e374ccccf386cffd4b479e2b9db60538c14aa054
Suwanda Kurnia <63093353+suwandakurnia@users.noreply.github.com>
2020-04-05 01:02:23 +0700

    Add files via upload

---
+++ TREX Google Chrome/TREX Google Chrome/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace TREX_Google_Chrome
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Floor = new System.Windows.Forms.PictureBox();
+            this.TRex = new System.Windows.Forms.PictureBox();
+            this.Obstacle1 = new System.Windows.Forms.PictureBox();
+            this.Obstacle2 = new System.Windows.Forms.PictureBox();
+            this.ScoreText = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Floor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TRex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Obstacle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Obstacle2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Floor
+            // 
+            this.Floor.BackColor = System.Drawing.Color.Black;
+            this.Floor.Location = new System.Drawing.Point(-12, 452);
+            this.Floor.Name = "Floor";
+            this.Floor.Size = new System.Drawing.Size(652, 50);
+            this.Floor.TabIndex = 0;
+            this.Floor.TabStop = false;
+            // 
+            // TRex
+            // 
+            this.TRex.Image = global::TREX_Google_Chrome.Properties.Resources.running;
+            this.TRex.Location = new System.Drawing.Point(131, 385);
+            this.TRex.Name = "TRex";
+            this.TRex.Size = new System.Drawing.Size(44, 60);
+            this.TRex.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.TRex.TabIndex = 1;
+            this.TRex.TabStop = false;
+            // 
+            // Obstacle1
+            // 
+            this.Obstacle1.Image = global::TREX_Google_Chrome.Properties.Resources.obstacle_1;
+            this.Obstacle1.Location = new System.Drawing.Point(371, 405);
+            this.Obstacle1.Name = "Obstacle1";
+            this.Obstacle1.Size = new System.Drawing.Size(23, 46);
+            this.Obstacle1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Obstacle1.TabIndex = 2;
+            this.Obstacle1.TabStop = false;
+            this.Obstacle1.Tag = "Obstacle";
+            // 
+            // Obstacle2
+            // 
+            this.Obstacle2.Image = global::TREX_Google_Chrome.Properties.Resources.obstacle_2;
+            this.Obstacle2.Location = new System.Drawing.Point(534, 400);
+            this.Obstacle2.Name = "Obstacle2";
+            this.Obstacle2.Size = new System.Drawing.Size(50, 50);
+            this.Obstacle2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Obstacle2.TabIndex = 3;
+            this.Obstacle2.TabStop = false;
+            this.Obstacle2.Tag = "Obstacle";
+            // 
+            // ScoreText
+            // 
+            this.ScoreText.AutoSize = true;
+            this.ScoreText.Font = new System.Drawing.Font("Product Sans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreText.Location = new System.Drawing.Point(13, 13);
+            this.ScoreText.Name = "ScoreText";
+            this.ScoreText.Size = new System.Drawing.Size(86, 24);
+            this.ScoreText.TabIndex = 4;
+            this.ScoreText.Text = "Score- 0";
+            this.ScoreText.Click += new System.EventHandler(this.ScoreText_Click);
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.GameEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(618, 475);
+            this.Controls.Add(this.ScoreText);
+            this.Controls.Add(this.Obstacle2);
+            this.Controls.Add(this.Obstacle1);
+            this.Controls.Add(this.TRex);
+            this.Controls.Add(this.Floor);
+            this.Name = "Form1";
+            this.Text = "TREX Google Chrome";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Floor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TRex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Obstacle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Obstacle2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Floor;
+        private System.Windows.Forms.PictureBox TRex;
+        private System.Windows.Forms.PictureBox Obstacle1;
+        private System.Windows.Forms.PictureBox Obstacle2;
+        private System.Windows.Forms.Label ScoreText;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ TREX Google Chrome/TREX Google Chrome/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TREX_Google_Chrome
+{
+    public partial class Form1 : Form
+    {
+        bool jumping = false;
+        int JumpSpeed = 10;
+        int Force = 12;
+        int Score = 0;
+        int ObstacleSpeed = 10;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ResetGame();
+        }
+
+        private void ScoreText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameEvent(object sender, EventArgs e)
+        {
+            TRex.Top += JumpSpeed;
+            ScoreText.Text = "Score: " + Score;
+
+            if (jumping && Force < 0)
+            {
+                jumping = false;
+            }
+
+            if (jumping)
+            {
+                JumpSpeed = -12;
+                Force -= 1;
+            }
+
+            else
+            {
+                JumpSpeed = 12;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag == "Obstacle")
+                {
+                    x.Left -= ObstacleSpeed;
+
+                    if (x.Left + x.Width < -120)
+                    {
+                        x.Left = this.ClientSize.Width + rnd.Next(200, 800);
+                        Score++;
+                    }
+
+                    if (TRex.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        GameTimer.Stop();
+                        TRex.Image = Properties.Resources.dead;
+                        ScoreText.Text += " Press R to Restrart ";
+                    }
+                }
+            }
+
+            if (TRex.Top >= 380 && !jumping)
+            {
+                Force = 12;
+                TRex.Top = Floor.Top - TRex.Height;
+                JumpSpeed = 0;
+            }
+
+            if (Score >= 10)
+            {
+                ObstacleSpeed = 15;
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && !jumping)
+            {
+                jumping = true;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.R)
+            {
+                ResetGame();
+            }
+
+            if (jumping)
+            {
+                jumping = false;
+            }
+        }
+
+        public void ResetGame()
+        {
+            Force = 12;
+            TRex.Top = Floor.Top - TRex.Height;
+            JumpSpeed = 0;
+            jumping = false;
+            Score = 0;
+            ObstacleSpeed = 10;
+            ScoreText.Text = "Score : " + Score;
+            TRex.Image = Properties.Resources.running;
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag == "Obstacle")
+                {
+                    int position = rnd.Next(600, 1000);
+
+                    x.Left = 640 + (x.Left + position + x.Width * 3);
+                }
+            }
+
+            GameTimer.Start();
+        }
+    }
+}
